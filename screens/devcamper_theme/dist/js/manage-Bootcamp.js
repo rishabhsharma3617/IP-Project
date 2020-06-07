16f7397d8b40a67e4dff3d4f8c33238d30a1d626
Rishabh Sharma <rishabhsharma3617@gmail.com>
2020-06-07 23:43:07 +0530

    This is progres

--- screens/devcamper_theme/dist/js/manage-Bootcamp.js
+++ screens/devcamper_theme/dist/js/manage-Bootcamp.js
@@ -1,10 +1,28 @@
+
+var bootcamp = {}
+
+function init1()
+{
+    
+    document.getElementById('btitle').appendChild(document.createTextNode(`${bootcamp.data.name}`))
+    if(bootcamp.data.name === 'codemasters')
+    document.getElementById('brating').innerText = '8'
+    else
+    document.getElementById('brating').innerText = bootcamp.data.averageRating
+    document.getElementById('bplace').appendChild(document.createTextNode(`${bootcamp.data.location.city} , ${bootcamp.data.location.state}`))
+    document.getElementById('bcarriers').appendChild(document.createTextNode(`${bootcamp.data.careers[0]},${bootcamp.data.careers[1]},${bootcamp.data.careers[2]},`))
+
+
+
+}
 function init()
 {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      const bootcamp = JSON.parse(this.response)
+     bootcamp = JSON.parse(this.response)
       console.log(bootcamp)
+      init1()
     }
 
   };
